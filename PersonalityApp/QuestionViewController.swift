20d0a78e967905d2c34e088712376177131438e8
p.m.student <Bolden.Chance02@gmail.com>
2021-03-25 13:52:14 -0500

    Added questions and stuff

--- PersonalityApp/QuestionViewController.swift
+++ PersonalityApp/QuestionViewController.swift
@@ -9,25 +9,37 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    var questions: [Question] = [ Question(text: "Which do you prefer to eat?",
+                                           type: .question1,
+                                           answers: [Answer (text: "Chicken", type: .snail),
+                                                     Answer (text: "Burger", type: .cuttlefish),
+                                                     Answer (text: "Apples", type: .octopus),
+                                                     Answer (text: "Ramen", type: .kompi)
+                                                     
+                                                     
+                                                     
+                                           ]),
+                                  Question(text: "What would you rather be?",
+                                           type: .question2,
+                                            answers: [Answer (text: "A dictator", type: .snail),
+                                                     Answer (text: "A benevolent king", type: .cuttlefish),
+                                                     Answer (text: "A snail", type: .octopus),
+                                                     Answer (text: "Elkay EZH2O Enhanced Wall Mounted Filtered Water Bottle Refilling Station, Light Gray", type: .kompi)
+                                                     
+                                                     
+                                           ]),
+                                  
+                                  Question(text: "Where would you live?",
+                                           type: .question3,
+                                           answers: [Answer (text: "The abyss(Florida)", type: .snail),
+                                                     Answer (text: "England", type: .cuttlefish),
+                                                     Answer (text: "Cambodia", type: .octopus),
+                                                     Answer (text: "In space", type: .kompi)
+                                           ]),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var _: [Question] = [ Question(text: "Which do you prefer to eat? ",
-                                              type: .question1,
-                                              answers: [Answer (text: "Chicken", type: .snail),
-                                                        Answer (text: "Chicken", type: .snail),
-                                                        Answer (text: "Chicken", type: .snail),
-                                                        Answer (text: "Chicken", type: .snail)
-                                                                                                                
-                                                                                                                 
-                                                                                                                 
-        
-        
-        
-        ])
-        
-        
-        ]
     }
     
 
